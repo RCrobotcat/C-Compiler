e74e2d5527f2fb2bccd6c9612331da581c91b543
RCrobotcat <136684976+RCrobotcat@users.noreply.github.com>
2025-12-05 01:49:22 +0800

    modified test codes

--- hello.c
+++ hello.c
@@ -4,21 +4,31 @@
 
 #include <stdio.h>
 
-int fibonacci(int i)
-{
-    if (i <= 1)
-    {
+int fibonacci(int i) {
+    if (i <= 1) {
         return 1;
     }
     return fibonacci(i - 1) + fibonacci(i - 2);
 }
 
-int main()
-{
+int main() {
+    // must firstly declare variables at the beginning of a block
+    int x;
+    int a;
     int i;
+    char* str;
+
+    // then statements
+    str = "Hello, World!\n";
+    printf("%s", str);
+
+    x = 1;
+    a = x == 0 ? 10 : 20;
+    printf("a = %d\n", a);
+
     i = 0;
-    while (i <= 10)
-    {
+
+    while (i <= 10) {
         printf("fibonacci(%2d) = %d\n", i, fibonacci(i));
         i = i + 1;
     }
